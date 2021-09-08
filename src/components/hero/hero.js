ce87ceab8333dfa4c6db1061e19eff6d13ac3eb0
Alexander Ivanov <alexanderiv.sci@gmail.com>
2021-09-08 17:25:15 +0100

    sass integrated + stylings to the general webpage view

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,13 +5,15 @@ export default class Hero extends Component {
         super();
     }
 
-    render(){
-        return <div className="hero" style={{backgroundImage: `url(${this.props.langData.hero_1_image})`}}>
-            <h1 className='hero__title'>{this.props.langData.hero_1_title}</h1>
-            {/*{console.log(this.state.paras)}*/}
-            {/*{this.state.paras.map((par, i) => {*/}
-            {/*    return <p className='hero__par' key={i}>par</p>*/}
-            {/*})}*/}
-        </div>
+    render() {
+        return <section className="hero" style={{backgroundImage: `url(${this.props.langData.hero_1_image})`}}>
+                <div className="hero__title__container">
+                    <h1 className='hero__title'>{this.props.langData.hero_1_title}</h1>
+                </div>
+                {/*{console.log(this.state.paras)}*/}
+                {/*{this.state.paras.map((par, i) => {*/}
+                {/*    return <p className='hero__par' key={i}>par</p>*/}
+                {/*})}*/}
+            </section>
     }
 }
