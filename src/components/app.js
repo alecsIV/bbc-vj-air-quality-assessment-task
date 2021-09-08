722188b18d03756810fb5bdab9e274b5ff033de1
Alexander Ivanov <alexanderiv.sci@gmail.com>
2021-09-08 14:18:14 +0100

    simple language switcher implemented

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import {h, Component} from 'preact';
 import {IntlProvider, Text} from 'preact-i18n'
-import defaultLangDefinition from '../assets/lang/english.json'
+import defaultLangData from '../assets/lang/english.json'
 
 const testDef = {
     title: "hello this is title",
@@ -11,32 +11,33 @@ export default class App extends Component {
         super();
         this.state = {
             currentLanguage: 'english',
-            otherLanguage: 'hindi',
-            langDefinition: defaultLangDefinition,
+            languages: ['English', 'Hindi'],
+            langData: defaultLangData,
             citiesData: []
         }
         this.getCitiesData();
     }
 
-    changeLanguage = () => {
-        this.setState({currentLanguage: this.state.otherLanguage, otherLanguage: this.state.currentLanguage})
-        // if (this.state.currentLanguage === 'english') this.setState({currentLanguage: 'hindi'});
-        // else this.setState({currentLanguage: 'english'});
-        // this.refreshData(language)
+    changeLanguage = (event) => {
         // Call this function to change language
+        this.setState({currentLanguage: event.target.value});
+        this.refreshData()
     };
 
-    refreshData = (language) => {
-        console.log(language);
-        import(`../assets/lang/${language}.json`)
-            .then(langDefinition => this.setState({langDefinition}));
-        this.getCitiesData();
+    refreshData = () => {
+        console.log(this.state.currentLanguage);
+        // Once language is changed, data needs ot be refreshed
+        import(`../assets/lang/${this.state.currentLanguage}.json`)
+            .then((langData) => {
+                this.setState({langData})
+                this.getCitiesData();
+            });
     }
 
     getCitiesData = () => {
         let cities = [];
-        console.log(this.state.langDefinition);
-        Object.keys(this.state.langDefinition).forEach((key) => {
+        console.log(this.state.langData);
+        Object.keys(this.state.langData).forEach((key) => {
             if (key.match(/(compare-tabs_1_city_\d*_)/g)) {
                 const splitKeyArr = key.split("_");
                 const cityProp = splitKeyArr[splitKeyArr.length - 1];
@@ -44,7 +45,7 @@ export default class App extends Component {
                 if (!cities[cityId]) {
                     cities.push({});
                 }
-                cities[cityId][cityProp] = this.state.langDefinition[key];
+                cities[cityId][cityProp] = this.state.langData[key];
             }
         });
         console.log(cities);
@@ -55,10 +56,18 @@ export default class App extends Component {
         return <div className="app-container">
             <div id="app">
                 <p>test</p>
-                {this.state.citiesData.map(city => {
-                    return <button key={city.name}>{city.name}</button>
-                })}
-                <button onClick={() => this.changeLanguage(this.state.currentLanguage)}>Change language</button>
+                {
+                    this.state.citiesData.map(city => {
+                        return <button key={city.name}>{city.name}</button>
+                    })
+                }
+                <select onChange={(event) => this.changeLanguage(event)} value={this.state.currentLanguage}>
+                    {
+                        this.state.languages.map(language => {
+                            return <option key={language} value={language.toLowerCase()}>{language}</option>
+                        })
+                    }
+                </select>
             </div>
         </div>;
     }
