975d93e755f1760f67acbbbab111345c19c4af01
Alexander Ivanov <alexanderiv.sci@gmail.com>
2021-09-12 22:41:41 +0100

    helpers optimised and commented

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,12 @@
-function isIE11(){
-    return !!window.MSInputMethodContext && !!document.documentMode;
+// Check if browser is IE11
+function isIE11() {
+    // IF check needed for the pre-rendered html files
+    if (typeof window !== "undefined") {
+        return !!window.MSInputMethodContext && !!document.documentMode;
+    }
 }
 
+// Extract the cities data from the provided JSON file
 function getCitiesData(data) {
     let cities = [];
 
@@ -20,6 +25,7 @@ function getCitiesData(data) {
     return cities;
 }
 
+// Extract the all paragraphs data from the provided JSON file
 function getParasData(data) {
     let paras = [];
 
@@ -32,6 +38,7 @@ function getParasData(data) {
     return paras;
 }
 
+// Match an input value from a range to one in a different range, maintaining the same ration between the passed value and its range
 function normaliseVal(inputValue, oldMin, oldMax, newMin, newMax) {
     const oldRange = (oldMax - oldMin)
     const newRange = (newMax - newMin)
@@ -39,6 +46,7 @@ function normaliseVal(inputValue, oldMin, oldMax, newMin, newMax) {
     return (((inputValue - oldMin) * newRange) / oldRange) + newMin;
 }
 
+// Return the min and max of an object key
 function objectMinMax(array, key) {
     const max = Math.max(...array.map((object) => {
         return object[key];
